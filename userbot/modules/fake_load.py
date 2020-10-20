a10e80ba259a23bfa5e0f2d2477e30efffe0f028
AndrielFR <andrielkogama2@gmail.com>
2020-10-20 16:55:21 -0300

    PurpleBot: add fake load module
    Signed-off-by: AndrielFR <andrielkogama2@gmail.com>

---
+++ userbot/modules/fake_load.py
@@ -0,0 +1,325 @@
+ # Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+from asyncio import sleep
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.fl(?: |$)(.*)")
+async def f_load(event):
+    message = event.pattern_match.group(1)
+    await event.edit("`Start loading...`")
+    await sleep(1)
+    await event.edit("0%")
+    number = 1
+    await event.edit(str(number) + "%   ▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   █████████████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ██████████████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████████▊")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ███████████████▉")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████████")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████████▎")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████████▍")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████████▌")
+    number = number+ 1
+    await sleep(0.03)
+    await event.edit(str(number) + "%   ████████████████▌")
+    await sleep(1)
+    await event.edit("`Done!`")
+
+CMD_HELP.update(
+    {
+        "fake_load": ".fl\
+    \nStarts a fake load progress."
+    }
+)
