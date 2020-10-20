8ca48a65ea8db5fce8fc3b8bb7f3313e155b4a0b
AndrielFR <andrielkogama2@gmail.com>
2020-10-20 17:03:19 -0300

    PurpleBot: fake_load: translate to PT-BR
    Signed-off-by: AndrielFR <andrielkogama2@gmail.com>

--- userbot/modules/fake_load.py
+++ userbot/modules/fake_load.py
@@ -12,7 +12,7 @@ from userbot.events import register
 @register(outgoing=True, pattern="^.fl(?: |$)(.*)")
 async def f_load(event):
     message = event.pattern_match.group(1)
-    await event.edit("`Start loading...`")
+    await event.edit("`Carregamento iniciado...`")
     await sleep(1)
     await event.edit("0%")
     number = 1
@@ -313,13 +313,11 @@ async def f_load(event):
     await event.edit(str(number) + "%   ████████████████▌")
     number = number+ 1
     await sleep(0.03)
-    await event.edit(str(number) + "%   ████████████████▌")
-    await sleep(1)
-    await event.edit("`Done!`")
+    await event.edit(str(number) + "%   ████████████████▌\n`Carregamento completado!`")
 
 CMD_HELP.update(
     {
         "fake_load": ".fl\
-    \nStarts a fake load progress."
+    \nInicia um carregamento falso."
     }
 )
