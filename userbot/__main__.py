b490e3aceb3338462804834ef71c7da9f8bdd125
thewhiteharlot <49533379+thewhiteharlot@users.noreply.github.com>
2020-11-03 19:21:59 -0300

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,7 +26,7 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running PurpleBot [v4.0]")
+LOGS.info("You are running PurpleBot [v3.7]")
 
 LOGS.info(
     "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
