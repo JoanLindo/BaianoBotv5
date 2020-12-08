614c6bdf30cc53dfcf67cd8eacf6101a9f14729e
thewhiteharlot <49533379+thewhiteharlot@users.noreply.github.com>
2020-12-07 22:09:44 -0300

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -459,6 +459,7 @@ async def translateme(trans):
     else:
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         message = str(trans.pattern_match.group(1))
 
     if not message:
@@ -481,6 +482,16 @@ async def translateme(trans):
     except BaseException:
         source_lan = "(O Google não forneceu esta informação.)"
 =======
+        reply_text = translator.translate(deEmojify(message), dest=TRT_LANG)
+    except ValueError:
+        await trans.edit("Idioma de destino inválido.")
+        return
+>>>>>>> parent of 9d1dd42... Fix de tradução de kensurbot
+=======
+        await trans.edit("`Dê um texto ou responda a uma mensagem para traduzir!`")
+        return
+
+    try:
         reply_text = translator.translate(deEmojify(message), dest=TRT_LANG)
     except ValueError:
         await trans.edit("Idioma de destino inválido.")
