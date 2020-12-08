b865bbb55a12d95da5f4491b4164169eae6c8d00
thewhiteharlot <49533379+thewhiteharlot@users.noreply.github.com>
2020-12-07 22:08:39 -0300

    fix push

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -18,7 +18,7 @@ from urllib.parse import quote_plus
 
 from bs4 import BeautifulSoup
 from emoji import get_emoji_regexp
-from google_trans_new import LANGUAGES, google_translator
+from googletrans import LANGUAGES, Translator
 from gtts import gTTS
 from gtts.lang import tts_langs
 from requests import get
@@ -446,24 +446,31 @@ async def imdb(e):
         await e.edit("Insira um **nome de filme válido** k obgd")
 
 
-@register(outgoing=True, pattern=r"^\.trt(?: |$)([\s\S]*)")
+@register(outgoing=True, pattern=r"^.trt(?: |$)([\s\S]*)")
 async def translateme(trans):
-    """ For .trt command, translate the given text using Google Translate. """
-
-    if trans.is_reply and not trans.pattern_match.group(1):
-        message = await trans.get_reply_message()
-        message = str(message.message)
+    """ Para o comando .trt, traduz o texto fornecido usando o Google Translate. """
+    translator = Translator()
+    textx = await trans.get_reply_message()
+    message = trans.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
     else:
+<<<<<<< HEAD
         message = str(trans.pattern_match.group(1))
 
     if not message:
         return await trans.edit(
             "`Envie um texto ou responda a uma mensagem para traduzir!`"
         )
+=======
+        await trans.edit("`Dê um texto ou responda a uma mensagem para traduzir!`")
+        return
+>>>>>>> parent of 9d1dd42... Fix de tradução de kensurbot
 
-    await trans.edit("Processando...")
-    translator = google_translator()
     try:
+<<<<<<< HEAD
         reply_text = translator.translate(deEmojify(message), lang_tgt=TRT_LANG)
     except ValueError:
         return await trans.edit("Idioma de destino inválido.")
@@ -472,14 +479,22 @@ async def translateme(trans):
         source_lan = translator.detect(deEmojify(message))[1].title()
     except BaseException:
         source_lan = "(O Google não forneceu esta informação.)"
+=======
+        reply_text = translator.translate(deEmojify(message), dest=TRT_LANG)
+    except ValueError:
+        await trans.edit("Idioma de destino inválido.")
+        return
+>>>>>>> parent of 9d1dd42... Fix de tradução de kensurbot
 
-    reply_text = f"De: **{source_lan}**\nPara: **{LANGUAGES.get(TRT_LANG).title()}**\n\n{reply_text}"
+    source_lan = LANGUAGES[f"{reply_text.src.lower()}"]
+    transl_lan = LANGUAGES[f"{reply_text.dest.lower()}"]
+    reply_text = f"De **{source_lan.title()}**\nPara **{transl_lan.title()}:**\n\n{reply_text.text}"
 
     await trans.edit(reply_text)
     if BOTLOG:
         await trans.client.send_message(
             BOTLOG_CHATID,
-            f"Foram traduzidas {source_lan.title()} coisas de {transl_lan.title()} agora.",
+            f"Traduzido algumas {source_lan.title()} coisas para {transl_lan.title()} agora.",
         )
 
 
